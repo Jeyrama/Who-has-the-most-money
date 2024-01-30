ec7ea2e4222581b4e7a4efb4d09ad91bdae46000
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-29 20:20:08 -0800

    Update Who has the most money.js

--- Who has the most money.js
+++ Who has the most money.js
@@ -50,3 +50,11 @@ function mostMoney(students) {
 }
 
 // or
+
+function mostMoney(s) {
+  s.sort((x,y)=>sum(y)-sum(x));
+  if(s.length>1 && sum(s[0])==sum(s[1]))return 'all';
+  return s[0].name;
+}
+
+const sum = (s) => (s.fives*5)+(s.tens*10)+(s.twenties*20);
